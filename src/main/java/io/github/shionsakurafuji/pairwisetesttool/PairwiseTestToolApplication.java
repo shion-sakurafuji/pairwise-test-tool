e1b3242ff36545522f823bffe7b6f5dc3419ae2f
Shion Sakurafuji <238503995+shion-sakurafuji@users.noreply.github.com>
2026-09-06 22:36:27 +0900

    Initial Spring Boot setup

---
+++ src/main/java/io/github/shionsakurafuji/pairwisetesttool/PairwiseTestToolApplication.java
@@ -0,0 +1,13 @@
+package io.github.shionsakurafuji.pairwisetesttool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairwiseTestToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairwiseTestToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/shionsakurafuji/pairwisetesttool/PairwiseTestToolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.shionsakurafuji.pairwisetesttool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairwiseTestToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
